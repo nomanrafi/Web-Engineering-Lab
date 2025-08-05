0173502b3c38142b0669ccc30cde41d8ef65fe45
Abdulah Al Noman <100467115+nomanrafi@users.noreply.github.com>
2025-08-06 02:10:14 +0600

    myphp

---
+++ myphp/index.php
@@ -0,0 +1,14 @@
+<?php
+echo "Hello World!<br><br>";
+
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br><br>";
+
+$color = "red";
+echo "My car is " . $color . "<br><br>";
+
+$txt = "MyPHP";
+echo "I love $txt!";
+
+?>
